cee4e066351b173107e2d926758f0f7626f68dc0
William Bradshaw <will.bradshaw50@gmail.com>
2022-06-11 20:20:24 +1000

    added guys

--- js/homeHeader.js
+++ js/homeHeader.js
@@ -4,7 +4,8 @@ line.style.strokeDasharray = length;
 line.style.strokeDashoffset = length;
 window.addEventListener("scroll", drawLine);
 function drawLine() {
-  var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-  var draw = length * scrollpercent;
-  line.style.strokeDashoffset = length - draw;
+    var scrollRatio = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    console.log(scrollRatio);
+    var draw = length * scrollRatio;
+    line.style.strokeDashoffset = length - draw;
 }
