19f3857ac7e5d3ca52cd2403031eb6da392b438f
William Bradshaw <will.bradshaw50@gmail.com>
2022-06-11 22:08:15 +1000

    button

--- js/homeHeader.js
+++ js/homeHeader.js
@@ -5,7 +5,6 @@ line.style.strokeDashoffset = length;
 window.addEventListener("scroll", drawLine);
 function drawLine() {
     var scrollRatio = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-    console.log(scrollRatio);
     var draw = length * scrollRatio;
     line.style.strokeDashoffset = length - draw;
 }
