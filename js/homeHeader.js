cfdb89f5f75a410031b012026156d95d086f79e2
William Bradshaw <will.bradshaw50@gmail.com>
2022-06-12 14:44:49 +1000

    added fade in

--- js/homeHeader.js
+++ js/homeHeader.js
@@ -1,8 +1,25 @@
 var line = document.getElementById("line");
+var feature = document.getElementById("feature");
 var length = line.getTotalLength();
 line.style.strokeDasharray = length;
 line.style.strokeDashoffset = length;
 window.addEventListener("scroll", drawLine);
+var amount = (-(window.innerHeight/2)+(window.innerHeight/3.7) + "px")
+
+const guys = document.querySelectorAll(".scroll");
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("reveal", entry.isIntersecting)
+    })
+},
+{
+    rootMargin: amount
+})
+
+guys.forEach(guy => {
+    observer.observe(guy)
+})
+
 function drawLine() {
     var scrollRatio = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
     var draw = length * scrollRatio;
