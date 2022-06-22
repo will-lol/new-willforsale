442136d6f3bdf04bc34753d231afb4ab658f8429
will <65345408+will-lol@users.noreply.github.com>
2022-06-22 13:47:54 +1000

    Update homeHeader.js

--- js/homeHeader.js
+++ js/homeHeader.js
@@ -24,4 +24,5 @@ function drawLine() {
     var scrollRatio = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
     var draw = length * scrollRatio;
     line.style.strokeDashoffset = length - draw;
+    console.log(scrollRatio);
 }
