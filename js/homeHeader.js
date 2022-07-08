ef1bf8a07f965d5a8532f6772eda6fafb6f49e14
William Bradshaw <will.bradshaw50@gmail.com>
2022-07-08 21:42:28 +1000

    kh

--- js/homeHeader.js
+++ js/homeHeader.js
@@ -1,13 +1,7 @@
-var line = document.getElementById("line");
-var feature = document.getElementById("feature");
-var length = line.getTotalLength();
-line.style.strokeDasharray = length;
-line.style.strokeDashoffset = length;
-window.addEventListener("scroll", drawLine);
 var amount = (-(window.innerHeight/2)+(window.innerHeight/3.7) + "px")
 
 const guys = document.querySelectorAll(".homeHeader__feature__guy");
-const observer = new IntersectionObserver(entries => {
+const observerGuys = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.toggle("reveal", entry.isIntersecting)
     })
@@ -17,18 +11,32 @@ const observer = new IntersectionObserver(entries => {
 })
 
 guys.forEach(guy => {
-    observer.observe(guy)
+    observerGuys.observe(guy)
 })
 
-function drawLine() {
-    requestAnimationFrame(drawLineFrame)
-}
+var feature = document.getElementById("feature");
+var line = document.getElementById("line");
+var length = line.getTotalLength();
+var draw, length;
+line.style.strokeDasharray = length;
+line.style.strokeDashoffset = length;
+var clientHeight = document.documentElement.clientHeight;
+window.addEventListener("scroll", function(){
+    requestAnimationFrame(drawLine);
+});
 
-function drawLineFrame() {
-    var scrollRatio = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+function drawLine() {
+    var scrollRatio = (document.documentElement.scrollTop) / (document.documentElement.scrollHeight - clientHeight);
     if (scrollRatio > 1) {
         scrollRatio = 1;
     }
-    var draw = length * scrollRatio;
+    draw = length * scrollRatio;
     line.style.strokeDashoffset = length - draw;
 }
+
+observerNav = new IntersectionObserver(hideElement => {
+    document.getElementById("scrollingText").classList.toggle("hideY", entry.isIntersecting);
+    
+}, {
+    threshold: 0.1
+})
